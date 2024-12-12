493fabf5f08e05fb3ffed1a900fc0e4f9640594b
Junaid <133190121+Junaid-Ahmadd@users.noreply.github.com>
2024-12-12 12:04:29 +0500

    Update InfiniteCanvas.svelte

--- src/lib/InfiniteCanvas.svelte
+++ src/lib/InfiniteCanvas.svelte
@@ -255,4 +255,11 @@
     </div>
     <div class="toolbar-right">
       <button on:click={onClose}>
-        <svg xmlns="http://www.w3.org/200
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+          <path d="M21 4.5l-6.5 6.5 6.5 6.5" />
+        </svg>
+        Close
+      </button>
+    </div>
+  </div>
+</div>
